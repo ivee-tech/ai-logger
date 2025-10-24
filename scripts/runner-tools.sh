2077be3a4bf6b85dc2d2fde2df25c9ef32bda613
Dan Radu <radudanielro@yahoo.com>
2025-10-24 11:29:38 +1100

    Adjusted build args; added run options for the ADO agent

--- scripts/runner-tools.sh
+++ scripts/runner-tools.sh
@@ -14,3 +14,23 @@ sudo apt-get update && \
 
 # verify
 dotnet --list-runtimes | grep 'Microsoft.AspNetCore.App 9.0'
+
+
+## execute the runner in background
+nohup ./run.sh &
+# to kill  the process triggered by nohup, identify the PID executing run.sh, then call kill <PID>
+ps aux | grep run.sh
+kill <PID> 
+# if the runners are still showing in GH runners dashboard, you may need to restart the GH runner VMs
+
+## run the GH runner as a service
+# install
+sudo ./svc.sh install
+# start
+sudo ./svc.sh start
+# check status
+sudo ./svc.sh status
+# stop
+sudo ./svc.sh stop
+# uninstall
+sudo ./svc.sh uninstall
